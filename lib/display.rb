58172740b55ae5fb47da2e3664a8892ac044645f
jmsmith1018 <jmsmith1018@gmail.com>
2021-11-30 16:21:12 -0700

    Add Display and Game classes with early pseudo code for how to design them

---
+++ lib/display.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+# Display Class that holds methods for outputting information in the CLI
+class Display
+  # Pseudo
+  # Welcome player to the game
+  def welcome
+    puts 'Welcome to a game of Hangman'
+  end
+end
---
+++ lib/game.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+# Game Class that holds logic for progressing a game of Hangman
+class Game
+  # Pseudo
+  # Init with dictionary word (inject this dependency)
+  # Include display
+  # How Game Should Work: the player gets 6 guesses at the word
+  # If the player guesses the word before they've used all 6 guesses, they win
+  # Else the player loses -- Have complete word displayed to them in this event.
+  # Method for revealing letters that the player guesses. Do this comparison here
+  #   for state purpose and have it passed to Display.
+  def placeholder
+    puts 'This is a placeholder'
+  end
+end
