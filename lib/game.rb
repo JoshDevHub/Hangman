eecf3da4ac2e2acf5ab206db846855c045ace6b7
jmsmith1018 <jmsmith1018@gmail.com>
2021-12-01 09:34:58 -0700

    Add method for revealing the encoded word based on a given letter

--- lib/game.rb
+++ lib/game.rb
@@ -10,14 +10,35 @@ class Game
   # How Game Should Work: the player gets 6 guesses at the word
   # If the player guesses the word before they've used all 6 guesses, they win
   # Else the player loses -- Have complete word displayed to them in this event.
-  # Method for revealing letters that the player guesses. Do this comparison here
-  #   for state purpose and have it passed to Display.
-  attr_reader :word
+  attr_reader :word, :encoded_word, :display
 
+  # TODO: inject class dependencies in main?
   def initialize
     @word = Dictionary.new.select_word
+    @encoded_word = encode_word
+    @display = Display.new
+  end
+
+  def game_loop
+    # Welcome player to the game
+    display.welcome
+  end
+
+  def encode_word
+    word.length.times.map { '_' }
+  end
+
+  # TODO: might not have to mutate here?
+  def reveal_letter(letter)
+    encoded_word.map!.with_index do |element, index|
+      if letter == word[index]
+        letter
+      else
+        element
+      end
+    end
   end
 end
 
 # method tests
-p Game.new.word
+my_game = Game.new
