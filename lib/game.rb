56a95b839f9ef60b29433527cfe2d917122c2b64
jmsmith1018 <jmsmith1018@gmail.com>
2021-12-01 08:59:51 -0700

    Create initializer for Game

--- lib/game.rb
+++ lib/game.rb
@@ -1,16 +1,23 @@
 # frozen_string_literal: true
 
+require_relative 'dictionary'
+require_relative 'display'
+
 # Game Class that holds logic for progressing a game of Hangman
 class Game
   # Pseudo
-  # Init with dictionary word (inject this dependency)
   # Include display
   # How Game Should Work: the player gets 6 guesses at the word
   # If the player guesses the word before they've used all 6 guesses, they win
   # Else the player loses -- Have complete word displayed to them in this event.
   # Method for revealing letters that the player guesses. Do this comparison here
   #   for state purpose and have it passed to Display.
-  def placeholder
-    puts 'This is a placeholder'
+  attr_reader :word
+
+  def initialize
+    @word = Dictionary.new.select_word
   end
 end
+
+# method tests
+p Game.new.word
