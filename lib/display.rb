fd95c21523f6b839473dad71ea2cfe01d23448a2
jmsmith1018 <jmsmith1018@gmail.com>
2021-12-01 10:05:46 -0700

    Add methods for displaying basic text in the CLI during the game

--- lib/display.rb
+++ lib/display.rb
@@ -7,4 +7,21 @@ class Display
   def welcome
     puts 'Welcome to a game of Hangman'
   end
+
+  def rules
+    puts <<~HEREDOC
+      You have 6 tries to guess the secret word.
+      Pick your letters wisely!
+    HEREDOC
+  end
+
+  def query_letter
+    puts 'Choose a letter to guess '
+  end
+
+  def display_secret_word(word)
+    puts word.join(' ')
+  end
 end
+
+Display.new.rules
