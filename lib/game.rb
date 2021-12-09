f03c4d77cf3e38ae3b6f7906ba731c426f614cac
jmsmith1018 <jmsmith1018@gmail.com>
2021-12-09 11:54:30 -0700

    Fix bugs with load game loops repeating

--- lib/game.rb
+++ lib/game.rb
@@ -7,7 +7,7 @@ require_relative 'user_input'
 
 # Game Class that holds logic for progressing a game of Hangman
 class Game
-  attr_reader :dictionary
+  attr_reader :dictionary, :save_path
 
   include Display
   include UserInput
@@ -15,20 +15,28 @@ class Game
 
   def initialize(dictionary)
     @dictionary = dictionary
+    @save_path = 'saved_games/saved_hangman.yaml'
   end
 
   def play_game
     puts welcome # TODO: Combine these display lines
     puts rules
-    if load_game? # FIXME: Bug with reusing loaded game if user plays again
-      file = load_game('saved_games/saved_hangman.yaml')
-      loaded_game = GameLoop.from_yaml(file)
-    end
+    loaded_game = load_game_method
     loop do
       game_loop = loaded_game || GameLoop.new(secret_word: dictionary.select_word)
       game_loop.run_game
+      loaded_game = nil
       break unless play_again?
     end
+  end
+
+  def load_game_method
+    return unless File.exist?(save_path)
+
+    return unless load_game?
+
+    file = load_game(save_path)
+    GameLoop.from_yaml(file)
   end
 
   def play_again?
--- lib/load_game.rb
+++ lib/load_game.rb
@@ -3,14 +3,22 @@
 # module for loading a saved game
 module LoadGame
   def load_game(file_path)
-    yaml_string = YAML.load File.read(file_path)
+    # binding.pry
+    if File.exist?(file_path)
+      YAML.load File.read(file_path)
+    else
+      puts 'A saved game does not exist. Starting a new game...'
+    end
   end
 
   def from_yaml(data)
-    # binding.pry
     new(secret_word: data[:@secret_word],
         incorrect_letters: data[:@incorrect_letters],
         correct_letters: data[:@correct_letters],
         incorrect_guesses: data[:@incorrect_guesses])
+  end
+
+  def delete_game(path_to_file)
+    File.delete(path_to_file) if File.exist?(path_to_file)
   end
 end
